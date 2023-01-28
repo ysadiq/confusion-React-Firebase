57cbe50449ebab53072fff40cfb09a5a31eda2dd
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-28 14:29:18 +0300

    React with Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCTLT8f8YisZ50yUribgdkEUUTu_d_hTYo",
+  authDomain: "confusionserver-c5ac5.firebaseapp.com",
+  databaseURL: "",
+  projectId: "confusionserver-c5ac5",
+  storageBucket: "confusionserver-c5ac5.appspot.com",
+  messagingSenderId: "682161112580"
+};
